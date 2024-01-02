011fe41052e50f010ee5c58531c5924f16b2aab3
iwannabeit <140404545+iwannabeit@users.noreply.github.com>
2024-01-02 22:50:34 +0900

    정적, MVC와 템플릿엔진, API practice

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -2,6 +2,8 @@ package hello.hellospring.controller;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -10,4 +12,35 @@ public class HelloController {
         model.addAttribute("data", "hello"); //data라는 곳에 넣은 값은 hello -> hello.index의 중괄호 부분에 대응된다.
         return "hello";
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name); //name = spring
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //http의 body 부분에 return 값을 직접 넣어주겠다.
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name; //"hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody //json으로 반환된다.
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello{
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name = name;
+        }
+    }
+
 }
