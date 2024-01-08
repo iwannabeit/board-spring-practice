ddd167bee922b9d383f648a56a362fe81e97b96e
iwannabeit <140404545+iwannabeit@users.noreply.github.com>
2024-01-08 20:05:04 +0900

    QnA simple board

---
+++ src/main/java/board/boardspring/Service/AnswerService.java
@@ -0,0 +1,23 @@
+package board.boardspring.Service;
+
+import board.boardspring.Domain.Answer;
+import board.boardspring.Domain.Question;
+import board.boardspring.Repository.AnswerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+    private final AnswerRepository answerRepository;
+
+    public void create(Question question, String content){
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        this.answerRepository.save(answer);
+    }
+}
